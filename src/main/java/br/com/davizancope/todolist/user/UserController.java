d8c9a800495de8edd99efc43fffdc8ef7e6e055c
Davi Zancope <davizancope@gmail.com>
2023-10-11 11:52:41 -0300

    Implementando a conexão com banco de dados e adicionando a classe de usuário

--- src/main/java/br/com/davizancope/todolist/user/UserController.java
+++ src/main/java/br/com/davizancope/todolist/user/UserController.java
@@ -2,6 +2,10 @@ package br.com.davizancope.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,8 +25,20 @@ public class UserController {
      * Void (sem retorno)
      */
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            // retorno status code 400 + Mensagem de erro no Body
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        // retorno status code 201 (criado) + obj no Body
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
